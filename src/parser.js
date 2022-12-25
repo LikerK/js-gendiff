09dfc3813b0ef896709432551c00d68f9004276e
LikerK <nagorniykirill@gmail.com>
2022-12-25 20:16:30 +0400

    refactoring

--- src/parser.js
+++ src/parser.js
@@ -2,11 +2,10 @@ import { readFileSync } from 'fs';
 import { resolve } from 'path';
 import { cwd } from 'process';
 
-
-export const getContent = (path1, path2) => {
-    const fullPathFile1 = resolve(cwd(), path1);
-    const fullPathFile2 = resolve(cwd(), path2);
-    const fileContent1 = JSON.parse(readFileSync(fullPathFile1));
-    const fileContent2 = JSON.parse(readFileSync(fullPathFile2));
-    return [fileContent1, fileContent2];
-}
+export default (path1, path2) => {
+  const fullPathFile1 = resolve(cwd(), path1);
+  const fullPathFile2 = resolve(cwd(), path2);
+  const fileContent1 = JSON.parse(readFileSync(fullPathFile1));
+  const fileContent2 = JSON.parse(readFileSync(fullPathFile2));
+  return [fileContent1, fileContent2];
+};
