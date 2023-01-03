4efcb8c0c47ed83e1611870e2b175ffdad677f56
LikerK <nagorniykirill@gmail.com>
2023-01-03 18:26:48 +0400

    update gendiff

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,14 +1,14 @@
 import { program } from 'commander';
-import genDiff from './diff.js';
+import gendiff from './engine.js';
 
 program
   .name('gendiff')
   .helpOption('-h, --help', 'output usage information')
   .version('output the version nmber')
   .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .argument('<filepath1>')
   .argument('<filepath2>')
-  .action((path1, path2) => console.log(genDiff(path1, path2)))
-  .option('-f, --format <type>', 'output format');
+  .action((path1, path2) => console.log(gendiff(path1, path2, program.opts().format)));
 
 program.parse(process.argv);
